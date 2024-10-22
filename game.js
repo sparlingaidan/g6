5bb091e53b0084d22379dc420591e8f3b7ef0e76
Ryan-Zimmerman1 <zimme4rm@dukes.jmu.edu>
2024-10-22 18:53:11 -0400

    Relocated container functionality
    Container is now drawn from the run() method in the Game class.

--- game.js
+++ game.js
@@ -12,6 +12,7 @@ class Game {
     //This draws game elements (just the background for now).
     run() {
         background(gameBackground);
+        this.loadContainer();
     }
 
     //Set the running state to true
@@ -37,5 +38,25 @@ class Game {
     getRunningState() {
         return this.running;
     }
+
+    // Function to draw the container
+    loadContainer() {
+        strokeWeight(1);
+        let c = [173, 216, 230];
+        let c2 = [136, 231, 136];
+  
+        fill(c);
+        rect(100, 449, 399, 50); // Base of container (blue rect)
+        fill(c2);
+        rect(100, 440, 399, 10); // Actual floor of container (green)
+        //x values for bounds in future: left:(100) right:(500)
+        //y values for bounds in future: top: 150 right: 449
+  
+        noFill();
+        strokeWeight(3);
+        line(100, 150, 100, 500);  // Left side line
+        line(500, 150, 500, 500);  // Right side line
+        line(100, 499, 500, 500);  // Bottom line
+  }
         
 }
--- sketch.js
+++ sketch.js
@@ -15,9 +15,7 @@ function draw() {
   if (instance.getRunningState() == true) { //If the game has started,
     instance.run();                         //draw the game
   } else {                                  //otherwise draw the menu.
-    background(200);                        //Main menu draw goes here
-    loadContainer();                        //This should be put in the if statement above, 
-                                            //but since we haven't gotten that working im loading it here
+    background(200);                        //Main menu draw goes here                   
   }
   if (settings) {                           //If settings is opened draw settings menu.
     fill('white');
@@ -45,22 +43,3 @@ function mousePressed() {
   }
 }
 
-// Function to draw the container
-function loadContainer() {
-  strokeWeight(1);
-  let c = [173, 216, 230];
-  let c2 = [136, 231, 136];
-
-  fill(c);
-  rect(100, 449, 399, 50); // Base of container (blue rect)
-  fill(c2);
-  rect(100, 440, 399, 10); // Actual floor of container (green)
-  //x values for bounds in future: left:(100) right:(500)
-  //y values for bounds in future: top: 150 right: 449
-
-  noFill();
-  strokeWeight(3);
-  line(100, 150, 100, 500);  // Left side line
-  line(500, 150, 500, 500);  // Right side line
-  line(100, 499, 500, 500);  // Bottom line
-}
