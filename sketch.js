2f0fc5c4cde7794bdb99640e812e43886170b964
marc <133734442+mtrain5@users.noreply.github.com>
2024-10-22 18:40:29 -0400

    added container

--- sketch.js
+++ sketch.js
@@ -3,7 +3,7 @@ let gameBackground; //The background for after the game has started
 var settings = false; //toggle for settings menu
 
 function setup() {
-  createCanvas(200, 200);
+  createCanvas(600, 500);
   textAlign(CENTER, TOP); //Makes it easier to place text.
 }
 
@@ -16,6 +16,8 @@ function draw() {
     instance.run();                         //draw the game
   } else {                                  //otherwise draw the menu.
     background(200);                        //Main menu draw goes here
+    loadContainer();                        //This should be put in the if statement above, 
+                                            //but since we haven't gotten that working im loading it here
   }
   if (settings) {                           //If settings is opened draw settings menu.
     fill('white');
@@ -41,4 +43,24 @@ function mousePressed() {
     settings = false; //close settings button
     }
   }
+}
+
+// Function to draw the container
+function loadContainer() {
+  strokeWeight(1);
+  let c = [173, 216, 230];
+  let c2 = [136, 231, 136];
+
+  fill(c);
+  rect(100, 449, 399, 50); // Base of container (blue rect)
+  fill(c2);
+  rect(100, 440, 399, 10); // Actual floor of container (green)
+  //x values for bounds in future: left:(100) right:(500)
+  //y values for bounds in future: top: 150 right: 449
+
+  noFill();
+  strokeWeight(3);
+  line(100, 150, 100, 500);  // Left side line
+  line(500, 150, 500, 500);  // Right side line
+  line(100, 499, 500, 500);  // Bottom line
 }
